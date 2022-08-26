b2420d60f0793680fc3c1f6dd35f5561c7faa8f9
Javier Lansky <jmlansky@hotmail.com>
2022-08-26 11:26:16 +0200

    add change

--- TestApi/TestApi/Controllers/WeatherForecastController.cs
+++ TestApi/TestApi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace TestApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+           "HELLOOOOOO", "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
